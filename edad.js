ac23e08fb75aff24e3b1e15c3fc53b915017b20e
paul romero <pavl64@hotmail.com>
2023-04-26 23:36:56 -0500

    mejorando la primera tarea de edades

--- edad.js
+++ edad.js
@@ -1,4 +1,15 @@
-let añoNacimiento = prompt("Ingresa tu año de nacimiento:");
-const añoActual = 2023;
-let edad = añoActual - añoNacimiento;
-console.log("Tu edad actual es: " + edad + " años.");
+let edad=0;
+const anioActual = 2023;
+let anioNacimiento = prompt("Ingresa tu año de nacimiento:");
+if(!isNaN(anioNacimiento)){
+    if(anioNacimiento<2024){
+        let edad =anioActual - anioNacimiento;
+        console.log("Tu edad actual es: " + edad + " años.");
+    }else{
+        console.log("aun no naces")
+    }
+}else{
+    console.log("debes colocar un numero")
+}
+
+
--- function sumar(num1, num2) {.js
+++ function sumar(num1, num2) {.js
@@ -1,5 +1,7 @@
 let num1 = prompt("Ingresa el primer numero:");
+num1=Number(num1);
 let num2 = prompt("Ingresa el segundo numero:");
+num2=Number(num2);
 function sumar(num1, num2) {
     return num1 + num2;
 }
@@ -16,7 +18,7 @@ function dividir(num1, num2) {
     return num1 / num2;
 }
 
-console.log("La suma de 30 y 15 es:", sumar(30, 15));
-console.log("La resta de 30 y 15 es:", restar(30, 15));
-console.log("La multiplicación de 30 y 15 es:", multiplicar(30, 15));
-console.log("La división de 30 y 15 es:", dividir(30, 15));
+console.log("La suma de "+num1+" y "+num2+" es:", sumar(num1,num2));
+console.log("La resta de "+num1+" y "+num2+" es:", restar(num1,num2));
+console.log("La multiplicación de "+num1+" y "+num2+" es:", multiplicar(num1,num2));
+console.log("La división de "+num1+" y "+num2+" es:", dividir(num1,num2));
